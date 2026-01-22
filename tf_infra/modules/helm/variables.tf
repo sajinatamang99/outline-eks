629fe740d039ea1370348f1c37390fbba962fd2a
sajinatamang99 <167822180+sajinatamang99@users.noreply.github.com>
2026-01-22 09:05:04 +0000

    Update variables.tf

--- tf_infra/modules/helm/variables.tf
+++ tf_infra/modules/helm/variables.tf
@@ -1,3 +1,14 @@
+variable "eks_cluster_name" {
+  type        = string
+  description = "EKS cluster name"
+}
+
+variable "eks_cluster_endpoint" {
+  type        = string
+  description = "EKS cluster API endpoint"
+}
+
 variable "external_dns_role_arn" {
-  type = string
+  type        = string
+  description = "IAM role ARN for external-dns"
 }
