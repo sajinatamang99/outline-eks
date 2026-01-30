7677fa0b68965dee617052dc38700fafb56eecce
sajinatamang99 <167822180+sajinatamang99@users.noreply.github.com>
2026-01-30 22:38:59 +0000

    deploy all resources

--- tf_infra/locals.tf
+++ tf_infra/locals.tf
@@ -1,14 +1,10 @@
-data "aws_route53_zone" "this" {
-  name = "devopsproject.org"
-}
-
 locals {
   name = "eks-cluster"
   # domain = "devopsproject.org"
   region           = "us-east-1"
   azs              = ["us-east-1a", "us-east-1b"]
   vpc_cidr         = "10.0.0.0/16"
-  hosted_zones_arn = data.aws_route53_zone.this.arn
+  hosted_zones_arn = "arn:aws:route53:::hostedzone/Z015437421LGNPHC43B54"
 
   tags = {
     project = "outline"
