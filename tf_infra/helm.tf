375867cf9982e44467cb2d7a1c4680a40ebdef59
sajinatamang99 <167822180+sajinatamang99@users.noreply.github.com>
2026-01-30 23:36:23 +0000

    deploy all resources

--- tf_infra/helm.tf
+++ tf_infra/helm.tf
@@ -31,7 +31,7 @@ resource "helm_release" "cert_manager" {
 
 resource "helm_release" "external_dns" {
   name       = "external-dns"
-  repository = "oci://registry-1.docker.io/bitnamicharts/external-dns"
+  repository = "https://charts.bitnami.com/bitnami"
   chart      = "external-dns"
 
   create_namespace = true
