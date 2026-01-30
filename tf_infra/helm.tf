d3d862134d9d20f8dc0e64ff366225ae5e31b79e
sajinatamang99 <167822180+sajinatamang99@users.noreply.github.com>
2026-01-30 23:54:36 +0000

    deploy all resources

--- tf_infra/helm.tf
+++ tf_infra/helm.tf
@@ -31,9 +31,9 @@ resource "helm_release" "cert_manager" {
 
 resource "helm_release" "external_dns" {
   name       = "external-dns"
-  repository = "https://charts.bitnami.com/bitnami"
+  repository = "https://kubernetes-sigs.github.io/external-dns/"
   chart      = "external-dns"
-  version    = "9.0.3"
+  version    = "5.6.0"
 
   create_namespace = true
   namespace        = "external-dns"
