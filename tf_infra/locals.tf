0f7e80134957bba0e3fefcc67d818ac11ed128e6
sajinatamang99 <167822180+sajinatamang99@users.noreply.github.com>
2026-02-01 15:04:08 +0000

    Feat4 (#7)
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources
    
    * deploy all resources

---
+++ tf_infra/locals.tf
@@ -0,0 +1,14 @@
+locals {
+  cluster_name  = "eks-cluster"
+  vpc_name      = "eks-vpc"
+  ecr_repo_name = "outline_repo"
+  # domain = "devopsproject.org"
+  region           = "us-east-1"
+  azs              = ["us-east-1a", "us-east-1b"]
+  vpc_cidr         = "10.0.0.0/16"
+  hosted_zones_arn = "arn:aws:route53:::hostedzone/Z015437421LGNPHC43B54"
+
+  tags = {
+    project = "outline"
+  }
+}
---
+++ tf_infra/providers.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_version = ">= 1.12.2"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.15.0, < 7.0" # my eks terraform module requires 6.15.0
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.1"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "eksproject-prodbackend"
+    key          = "tf_infra/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true #S3 native locking
+  }
+}
+
+provider "aws" {
+  region = local.region
+}
+
+# Rolling new tokens for cluster certificate
+provider "helm" {
+  kubernetes = {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec = {
+      api_version = "client.authentication.k8s.io/v1"
+      args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
+      command     = "aws"
+    }
+  }
+}
